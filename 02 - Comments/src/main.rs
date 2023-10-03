e700149913a71fbdfc42cd39c6db8e7d23588c73
Rick Beerendonk <rick@beerendonk.com>
2023-10-03 22:57:23 +0200

    Add "Comments" example.

---
+++ 02 - Comments/src/main.rs
@@ -0,0 +1,25 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+fn main() {
+	
+	// One line
+
+	println!("1");
+
+	/*
+	Multi
+	line
+	*/
+
+	println!("2");
+
+	/*
+	Multi
+        // Nested
+        /* Nested */
+	line
+	*/
+
+	println!("3");
+}
