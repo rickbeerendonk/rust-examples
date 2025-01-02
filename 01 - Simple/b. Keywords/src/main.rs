2870591aaf18da074f957070aae1eaf63207ca49
Rick Beerendonk <rick@beerendonk.com>
2025-01-02 21:04:47 +0100

    Add "Simple - Keywords" example.

---
+++ 01 - Simple/b. Keywords/src/main.rs
@@ -0,0 +1,54 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+// Keywords in Rust
+// https://doc.rust-lang.org/reference/keywords.html
+
+// as          break       const       continue    crate
+// else        enum        extern      false       fn
+// for         if          impl        in          let
+// loop        match       mod         move        mut
+// pub         ref         return      self        Self
+// static      struct      super       trait       true
+// type        unsafe      use         where       while
+// async       await       dyn         abstract    become
+// box         do          final       macro       override
+// priv        try         typeof      unsized     virtual
+// yield
+
+// Keywords
+
+// as          break       const       continue    crate
+// else        enum        extern      false       fn
+// for         if          impl        in          let
+// loop        match       mod         move        mut
+// pub         ref         return      self        Self
+// static      struct      super       trait       true
+// type        unsafe      use         where       while
+// async       await       dyn         abstract    become
+// box         do          final       macro       override
+// priv        try         typeof      unsized     virtual
+// yield
+
+// Reserved keywords
+// alignof     auto        become      concept     co_await
+// co_return   coyield     default     do          double
+// extern      goto        inline      let         macro
+// mutable     nil         override    partial      pragma
+// register     restrict    sizeof      static_assert
+// static_cast  struct      template     thread_local
+// typeid      typename    typeof      union       using
+// virtual     where       while       with        yield
+//
+// Keywords that are not reserved
+// abstract    alignof     auto        become      box
+// break       const       continue    crate       do
+// double      else        enum        extern      false
+// final       for         goto        if          impl
+// in          inline      let         macro       move
+// mut         nil         override    partial      pragma
+// pub         ref         register    return      self
+// static      struct      super       switch      template
+// this        thread_local type        typeof      true
+// typeid      typename    union       unsafe      use
+// virtual     void        volatile    where       while
